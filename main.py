28dd6df36a5862ecf4e34fe426a60303de9ac0df
chayane-kha <chayane.kha@edu.esiee.fr>
2024-11-08 14:45:27 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+"""pas de module importe"""
 #### Fonction secondaire
 
 
 def ispalindrome(p):
+    """
+    Retourne True si p est un palindrome sinon retourne False
+    """
 
     # votre code ici
-    
-    return False
+    pmin = p.lower()
+    pmin = pmin.replace(" ", "")
+    pmin = pmin.replace("'", "")
+    pmin = pmin.replace(",", "")
+    pmin = pmin.replace("!", "")
+    pmin = pmin.replace("?", "")
+    pmin = pmin.replace(":", "")
+    pmin = pmin.replace("-", "")
+    accent = "éàçëêèûüùô"
+    sans_accent = "eaceeeuuuo"
+    trans = str.maketrans(accent, sans_accent)
+    pmin = pmin.translate(trans)
+
+    for i in range((int)(len(p)/2)):
+        if pmin[i] != pmin[-(i+1)]:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
+    """
+    Fonction principale
+    """
 
     # vos appels à la fonction secondaire ici
 
